Fixed dataclass and base pointnet and added normalization

AutoAnnSmoother/smoother/data/common/sequence_data.py
@@ -17,7 +17,6 @@ class TrackingResults():
         # CONFIG SPECIFIC
         self.feature_dim = self.config["data"]["feature_dim"]
         self.window_size = self.config["data"]["window_size"]
-        self.max_tracks = self.config["data"]["max_tracks"]
         self.gt_dim = self.config["data"]["gt_dim"]
         self.gt_assoc_threshold = self.config["data"]["gt_assoc_threshold"]
 
@@ -64,9 +63,8 @@ class SequenceData():
         self.start_ind, self.end_ind = window
 
         self.feature_dim = self.tracking_results.feature_dim
-        self.window_size = self.end_ind - self.start_ind
+        self.window_size = self.end_ind - self.start_ind + 1
         self.foi_index = foi_index
-        self.max_tracks = self.tracking_results.max_tracks
         self.gt_dim = self.tracking_results.gt_dim
         self.gt_assoc_threshold = self.tracking_results.gt_assoc_threshold
 
@@ -83,51 +81,42 @@ class SequenceData():
         gt_assoc = self._get_corresponding_foi_gt_boxes(foi_token) #get all gt boxes for foi for all boxes
 
         track_id_pc = defaultdict(list)
-        for i, frame_token in enumerate(window_tokens, self.start_ind):
+        for i, frame_token in enumerate(window_tokens):
             frame_pred_boxes = self.tracking_results.get_pred_boxes_from_frame(frame_token)
             for box in frame_pred_boxes:
+                if not box.tracking_id in track_id_pc:
+                    t_encs = [i+self.start_ind-foi_ind for i in range(self.window_size)]
+                    init_point = [[0]*(self.feature_dim-1) + [t_encs[i]] for i in range(self.window_size)]
+                    track_id_pc[box.tracking_id] = init_point
+
                 center = list(box.translation)
                 size = list(box.size)
                 rotation = list(box.rotation)
-                temp_encoding = [i-foi_ind]
+                temp_encoding = [i+self.start_ind-foi_ind]
                 point = center + size + rotation + temp_encoding
-                track_id_pc[box.tracking_id].append(point)
+                track_id_pc[box.tracking_id][i] = point
 
         x, y = [], []
         for track_id, point_cloud in track_id_pc.items():
-            if track_id in gt_assoc:
-                if gt_assoc[track_id]:
-                    gt_box = self._get_gt_box(gt_assoc, track_id)
-                else:
-                    gt_box = [0]*self.gt_dim
-
-                #if k in gt_assoc:
-                pad = [[0]*self.feature_dim] * (self.window_size-len(point_cloud))
-                padded_pc = point_cloud + pad
-                x.append(padded_pc)
+            if track_id in gt_assoc: # track_id2 
+                gt_box = self._get_gt_box(gt_assoc, track_id)
+                x.append(point_cloud)
                 y.append(gt_box)
-
-        x, y = torch.tensor(x), torch.tensor(y)
-
-        #Pad or truncate
-        if len(x) < self.max_tracks:
-            x = F.pad(x,(0,0,0,0,0,self.max_tracks-len(x)))
-        else:
-            x = x[:self.max_tracks,:,:]
-
-        if len(y) < self.max_tracks:
-            y = F.pad(y,(0,0,0,self.max_tracks-len(y)))
-        else:
-            y = y[:self.max_tracks,:] 
 
         return x,y
 
     def _get_gt_box(self, gt_assoc, track_id):
-        box = gt_assoc[track_id]
-        center = list(box.translation)
-        size = list(box.size)
-        rotation = list(box.rotation)
-        return center + size + rotation
+        if gt_assoc[track_id] is None:
+            point = [0]*self.gt_dim
+        else:
+            box = gt_assoc[track_id]
+            center = list(box.translation)
+            size = list(box.size)
+            rotation = list(box.rotation)
+            exist = [1]
+            point = center + size + rotation + exist
+            
+        return point
 
     def _get_frames_in_sequence(self, scene_token):
         seq = self.tracking_results.get_sequence_from_id(scene_token)
@@ -153,6 +142,7 @@ class SequenceData():
         if len(frame_gt_boxes) == 0:
             for pred_idx, pred_box in enumerate(frame_pred_boxes):
                 gt_track_id_assocs[pred_box.tracking_id] =  None
+            
             return gt_track_id_assocs
 
         dists = l2(frame_gt_boxes, frame_pred_boxes)
@@ -171,14 +161,23 @@ class SequenceData():
     
 class WindowTracksData():
 
-    def __init__(self, tracking_results: TrackingResults, window):
+    def __init__(self, tracking_results: TrackingResults, window, means = None, stds = None):
         self.tracking_results = tracking_results
         self.window = window
+        self.means = torch.tensor(means) if means else None
+        self.stds = torch.tensor(stds) if stds else None
 
         self.data = list(self._get_data_samples())
 
     def __getitem__(self, index):
-        return self.data[index]
+        track, track_gt = self.data[index]
+
+        if self.means is not None and self.stds is not None:
+           track = (track - self.means)/self.stds
+           n_norm_features = len(track[0])
+           track_gt[:n_norm_features] = (track_gt[:n_norm_features] - self.means) / self.stds
+
+        return (track, track_gt)
 
     def __len__(self):
         return len(self.data)
@@ -187,17 +186,17 @@ class WindowTracksData():
         seq_data = SequenceData(self.tracking_results, self.window)
         for x_seq, y_seq in seq_data:
             for track, track_gt in zip(x_seq,y_seq):
-                is_tracks = torch.count_nonzero(track) != torch.tensor(0)
-                is_gt_track = torch.count_nonzero(track_gt) != torch.tensor(0)
-                if is_tracks and is_gt_track:
-                    yield (track, track_gt)
+                track, track_gt = torch.tensor(track, dtype=torch.float32), torch.tensor(track_gt, dtype=torch.float32)
+                yield (track, track_gt)
 
 class SlidingWindowTracksData():
 
-    def __init__(self, tracking_results: TrackingResults, window_size=5, foi_index=20):
+    def __init__(self, tracking_results: TrackingResults, window_size=5, foi_index=20, means=None, stds=None):
         self.tracking_results = tracking_results
         self.window_size = window_size
         self.foi_index = foi_index
+        self.means = means
+        self.stds = stds
         self.data = list(self._get_data_samples())
 
     def __getitem__(self,index):
@@ -211,8 +210,9 @@ class SlidingWindowTracksData():
             if self.foi_index - self.window_size + i < 0:
                 continue
             start_ind = self.foi_index-self.window_size + i + 1
-            end_ind = start_ind + self.window_size
+            end_ind = start_ind + self.window_size - 1
             window = (start_ind, end_ind)
-            wind_track_nusc = WindowTracksData(self.tracking_results,window)
+            print(window)
+            wind_track_nusc = WindowTracksData(self.tracking_results,window,self.means, self.stds)
             for track in wind_track_nusc:
                 yield track

AutoAnnSmoother/smoother/models/pointnet.py
@@ -1,9 +1,9 @@
 from torch import nn
 
-class WaymoPointNet(nn.Module):
+class PointNet(nn.Module):
 
     def __init__(self, input_dim=11, out_size=11, mlp1_sizes=[64,64,128,512], mlp2_sizes=[128,128], window_size=5):
-        super(WaymoPointNet, self).__init__()
+        super(PointNet, self).__init__()
         self.input_dim = input_dim
 
         self.mlp1 = nn.Sequential(
@@ -12,28 +12,28 @@ class WaymoPointNet(nn.Module):
                         nn.Linear(mlp1_sizes[1], mlp1_sizes[2]),
                         nn.Linear(mlp1_sizes[2], mlp1_sizes[3])
                     )
-        kernel_size = (1,16)
+        kernel_size = (window_size,1)
         stride = 1
         padding = 0
         self.max_pool = nn.MaxPool2d(kernel_size, stride, padding)
         
         self.mlp2 = nn.Sequential(
-                        nn.Linear(mlp1_sizes[3]-15, mlp2_sizes[0]),
+                        nn.Linear(mlp1_sizes[3], mlp2_sizes[0]),
                         nn.Linear(mlp2_sizes[0], mlp2_sizes[1])
                     )
 
-        self.decoder = nn.Sequential(
-                        nn.Conv1d(window_size,16,window_size),
-                        nn.MaxPool2d((16,1))
-                        )
-
-        self.fc_out = nn.Linear(mlp2_sizes[1]-window_size+1, out_size)
+        self.fc_out = nn.Linear(mlp2_sizes[1], out_size)
 
     def forward(self, x):
+        #print("x_in", x.shape)
         x = self.mlp1(x)
+        #print("x_enc", x.shape)
         x = self.max_pool(x)
-        x = self.mlp2(x)
-        x = self.decoder(x)
+        #print("x_max_pooled", x.shape)
         x = x.squeeze()
+        #print("x_squeezed", x.shape)
+        x = self.mlp2(x)
+        #print("x_dec", x.shape)
         x = self.fc_out(x)
+        #print("x_out", x.shape)
         return x

AutoAnnSmoother/tools/utils/smoothing_trainer.py
@@ -1,7 +1,7 @@
 from smoother.io.load_config import load_config
 from tools.utils.training_utils import Trainer
 from smoother.data.common.sequence_data import SlidingWindowTracksData, WindowTracksData
-from smoother.models.pointnet import WaymoPointNet
+from smoother.models.pointnet import PointNet
 import torch
 from torch import optim
 from torch.utils.data import random_split, DataLoader
@@ -23,6 +23,8 @@ class SmoothingTrainer():
         self.foi_index = self.conf["data"]["foi_index"] # index in sequence where annotation exist
         self.window_size = self.conf["data"]["window_size"]
         self.sliding_window = self.conf["data"]["sliding_window"]
+        
+        self.normalize = self.conf["data"]["normalize"]["normalize"]
 
         self.model_type = self.conf["model"]["type"]
 
@@ -57,14 +59,31 @@ class SmoothingTrainer():
         else:
             raise NotImplementedError(f"Dataclass of type {self.data_type} is not implemented. Please use 'nuscenes' or 'zod'.")
         
+        #Normalization
+        if self.normalize:
+            center_mean = self.conf["data"]["normalize"]["center"]["mean"]
+            center_stdev = self.conf["data"]["normalize"]["center"]["stdev"]
+            size_mean = self.conf["data"]["normalize"]["size"]["mean"]
+            size_stdev = self.conf["data"]["normalize"]["size"]["stdev"]
+            rotation_mean = self.conf["data"]["normalize"]["rotation"]["mean"]
+            rotation_stdev = self.conf["data"]["normalize"]["rotation"]["stdev"]
+            score_mean = self.conf["data"]["normalize"]["score"]["mean"]
+            score_stdev = self.conf["data"]["normalize"]["score"]["stdev"]
+
+            means = center_mean + size_mean + rotation_mean + score_mean
+            stdev = center_stdev + size_stdev + rotation_stdev + score_stdev
+        else:
+            means, stdev = None, None
+
+
         # Load data model
         if self.sliding_window:
-            self.data_model = SlidingWindowTracksData(self.tracking_results,self.window_size, self.foi_index)
+            self.data_model = SlidingWindowTracksData(self.tracking_results,self.window_size, self.foi_index, means, stdev)
         else:
-            start_ind = self.foi_index - (self.window_size-1)/2
-            end_ind = self.foi_index + (self.window_size-1)/2
+            start_ind = int(self.foi_index - (self.window_size-1)/2)
+            end_ind = int(self.foi_index + (self.window_size-1)/2)
             window = (start_ind, end_ind)
-            self.data_model = WindowTracksData(self.tracking_results,window)
+            self.data_model = WindowTracksData(self.tracking_results,window, means, stdev)
 
 
     def load_model(self):
@@ -74,7 +93,7 @@ class SmoothingTrainer():
             out_size = self.conf["model"][self.model_type]["out_size"]
             mlp1_sizes = self.conf["model"][self.model_type]["mlp1_sizes"]
             mlp2_sizes = self.conf["model"][self.model_type]["mlp2_sizes"]
-            self.model = WaymoPointNet(input_dim, out_size, mlp1_sizes, mlp2_sizes, self.window_size)
+            self.model = PointNet(input_dim, out_size, mlp1_sizes, mlp2_sizes, self.window_size)
 
 
     def train(self):
@@ -92,8 +111,8 @@ class SmoothingTrainer():
 
         batch_size = self.batch_size
 
-        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.n_workers)
-        val_dataloader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.n_workers)
+        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.n_workers)
+        val_dataloader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.n_workers)
 
         self.trained_model, train_losses, val_losses = trainer.training_loop(self.model,optimizer,loss_fn,self.n_epochs, train_dataloader, val_dataloader)
         self.result_dict = {"train_losses": train_losses, "val_losses":val_losses}

AutoAnnSmoother/tools/utils/training_utils.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 import numpy as np
 
 class Trainer():
@@ -43,7 +44,7 @@ class Trainer():
 
             tracks, gt_anns = x.to(device), y.to(device).long()
             optimizer.zero_grad()
-
+            #print("batch", tracks.shape, gt_anns.shape)
             model_output = model.forward(tracks)
 
             loss = loss_fn(model_output.view(-1, self.out_size) , gt_anns.float())
@@ -66,26 +67,39 @@ class Trainer():
                 val_loss_cum += loss.item()
 
         return val_loss_cum/len(val_loader)
+    
+    def compute_score_loss(self, predictions, gts):
+        scores = predictions[:,10]
+        gt_score = gts[:,10]
+        m = nn.Sigmoid()
+        loss = nn.BCELoss()
+        out = loss(m(scores),gt_score)
+        return out
+
+
 
     def compute_loss(self, predictions, gts):
+        #print("Loss", predictions.shape, gts.shape)
         centers = predictions[:,:3]
         gt_centers = gts[:,:3]
         center_loss = torch.linalg.norm(gt_centers-centers)
 
         sizes = predictions[:,3:6]
-        gt_sizes = gts[:,:3:6]
+        gt_sizes = gts[:,3:6]
         size_loss = torch.linalg.norm(gt_sizes-sizes)
 
         rotation = predictions[:,6:10]
-        gt_rotation = gts[:,:6:10]
+        gt_rotation = gts[:,6:10]
         rotation_loss = torch.linalg.norm(gt_rotation-rotation)
 
         return center_loss, size_loss, rotation_loss
 
     def box_regression_loss(self, predictions, gt):
         center_loss, size_loss, rotation_loss = self.compute_loss(predictions, gt)
+        score_loss = self.compute_score_loss(predictions,gt)
         center_loss *= self.loss_params["weight"]["center"]
         size_loss *= self.loss_params["weight"]["size"]
         rotation_loss *= self.loss_params["weight"]["rotation"]
-
-        return center_loss + size_loss + rotation_loss
+        score_loss *= self.loss_params["weight"]["score"]
+        #print(center_loss, size_loss, rotation_loss, score_loss)
+        return center_loss + size_loss + rotation_loss + score_loss