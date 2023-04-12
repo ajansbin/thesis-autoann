374918e898d6b0536b36a3614e930877a9dffa31
Amanda Allgurén <amanda.allguren@zenseact.com>
2023-04-12 12:53:17 +0200

    Added GIoU and FP scores to evaluation

--- Evaluator/tools/evaluate.py
+++ Evaluator/tools/evaluate.py
@@ -3,20 +3,40 @@ from zod.eval.detection._nuscenes_eval.common.data_classes import EvalBoxes
 from zod.eval.detection._nuscenes_eval.detection.data_classes import DetectionBox
 from smoother.data.zod_data import ZodTrackingResults
 from smoother.io.config_utils import load_config
-
+from tools.utils.evaluation import giou3d
+import numpy as np
 from zod.eval.detection.eval_nuscenes_style import evaluate_nuscenes_style
 
 import json
 
-def main(data_path:str, version:str, split:str, result_path:str, type:str, config:str, save_path:str):
+class_names = [
+    'Vehicle']#, 'VulnerableVehicle', 'Pedestrian']
+
+
+def main(data_path:str, version:str, split:str, result_path:str, type:str, config:str, save_path:str, eval_type:str):
     conf = load_config(config)
+    giou_thres = conf["data"]["association_thresholds"]['giou']
 
     tracking_results = ZodTrackingResults(result_path, conf, version, split, data_path)
 
-    evaluate_zod_nuscenes_style(tracking_results, type, save_path)
+    gtsEval, predEval = create_eval_boxes(tracking_results, type)
 
+    if eval_type == 'nuscenes':
+        evaluate_nuscenes_style(gtsEval, predEval, verbose=True, output_path=save_path)
+    elif eval_type == 'giou':
+        print(mean_giou(gtsEval, predEval, giou_thres))
+    elif eval_type == 'fp':
+        _, fp = calculate_gious_fp(gtsEval, predEval, giou_thres)
+        print('Number false positives:', fp)
+        print('Total number predicted boxes:', len(predEval.all))
+    elif eval_type == 'all':
+        evaluate_nuscenes_style(gtsEval, predEval, verbose=True, output_path=save_path)
+        giou, fp = calculate_gious_fp(gtsEval, predEval, giou_thres)
+        print('Mean GIoU', sum(giou)/len(giou))
+        print('Number false positives:', fp)
+        print('Total number predicted boxes:', len(predEval.all))
 
-def evaluate_zod_nuscenes_style(tracking_results, type, save_path):
+def create_eval_boxes(tracking_results, type):
     gts = tracking_results.gt_boxes
     gt_frames = tracking_results.gt_frames
     preds = tracking_results.pred_boxes
@@ -32,8 +52,8 @@ def evaluate_zod_nuscenes_style(tracking_results, type, save_path):
             translation = [box["translation"][0]] + [box["translation"][1]] + [gravity_center]
             score = box['detection_score'] if type == 'detection' else box['tracking_score']
             name = box['detection_name'] if type == 'detection' else box['tracking_name']
-            #if score < 0.001:
-            #    continue
+            if name not in class_names:
+                continue
             pred_box = DetectionBox(
                 sample_token=seq_id,
                 translation=tuple(translation),
@@ -48,9 +68,8 @@ def evaluate_zod_nuscenes_style(tracking_results, type, save_path):
             
         gt_boxes = []
         for box in gts[seq_id]:
-            if box['detection_name'] != 'Vehicle':
-                print(box['detection_name'])
-            
+            if box['detection_name'] not in class_names:
+                continue
             gt_box = DetectionBox(
                 sample_token=seq_id,
                 translation=tuple(box['translation']),
@@ -60,12 +79,49 @@ def evaluate_zod_nuscenes_style(tracking_results, type, save_path):
                 detection_name=box['detection_name'],
                 detection_score=box['detection_score'],
             )
-            #print('GT', gt_box)
-            gt_boxes.append(gt_box)
-            
+            gt_boxes.append(gt_box) 
         gtsEval.add_boxes(seq_id, gt_boxes)
 
-    evaluate_nuscenes_style(gtsEval, predEval, verbose=True, output_path=save_path)
+    return gtsEval, predEval
+
+def calculate_giou3d_matrix(gts, pred):
+    dists = np.zeros(len(gts))
+    for i, gt in enumerate(gts):
+        gt_array = list(gt.translation) + list(gt.size) + list(gt.rotation)
+        pred_array = list(pred.translation) + list(pred.size) + list(pred.rotation)
+        giou = giou3d(gt_array, pred_array)
+        dists[i] = giou
+    return dists
+
+def calculate_gious_fp(gtsEval, predEval, giou_thres):
+    gious = []
+    false_pos = 0
+
+    for seq_id in gtsEval.boxes.keys():
+        preds = predEval.boxes[seq_id]
+        gts = gtsEval.boxes[seq_id]
+        if len(gts) == 0:
+            print('No ground truth found for sequence', seq_id)
+            false_pos += 1  
+            continue
+        if len(preds) == 0:
+            print('No predictions found for sequence', seq_id)
+            continue
+        for track_box in preds:
+            dists = calculate_giou3d_matrix(gts, track_box)
+            gt_idx = np.argmax(dists)
+            gt_dist = np.max(dists)
+            valid_match = dists[gt_idx] >= giou_thres
+            if valid_match:
+                gious.append(gt_dist)
+            if not valid_match:
+                false_pos += 1 
+    return gious, false_pos
+
+
+def mean_giou(gts, preds, giou_thres):
+    gious, _ = calculate_gious_fp(gts, preds, giou_thres)
+    return sum(gious)/len(gious)
 
 if __name__ == '__main__':
     import argparse
@@ -76,9 +132,10 @@ if __name__ == '__main__':
 
     parser.add_argument('--result-path', type=str, default="/staging/agp/masterthesis/2023autoann/storage/detection/CenterPoint/predictions/cp-zod-mini-results-val/pts_bbox/results_zod.json", help="Path to results")
     parser.add_argument('--type', type=str, default='detection', choices=['detection', 'tracking', 'smoothing'])
+    parser.add_argument('--eval-type', type=str, default='detection', choices=['nuscenes', 'giou', 'fp', 'all'])
     parser.add_argument('--config', type=str, default='/home/s0001668/workspace/thesis-autoann/AutoAnnSmoother/configs/training_config.yaml')
     parser.add_argument('--save-dir', type=str, default="/home/s0001668/workspace/storage/smoothing/")
     
     args = parser.parse_args()
 
-    main(args.data_path, args.version, args.split, args.result_path, args.type, args.config, args.save_dir)
+    main(args.data_path, args.version, args.split, args.result_path, args.type, args.config, args.save_dir, args.eval_type)
